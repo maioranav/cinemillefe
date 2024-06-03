17f17ee15fc4eddc2523605ef87df004cbf4d0f4
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-03 20:52:29 +0200

    feat(public): implement footer

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NowfeaturedComponent } from './components/nowfeatured/nowfeatured.component';
 import { FilmcardComponent } from './components/filmcard/filmcard.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [NavbarComponent, NowfeaturedComponent, FilmcardComponent],
+  imports: [
+    NavbarComponent,
+    NowfeaturedComponent,
+    FilmcardComponent,
+    FooterComponent,
+  ],
 })
 export class HomeComponent {}
