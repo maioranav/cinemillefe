36ce8cd08a62ecb4e6e2c7f1d99a4ccb0a4fbc24
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-03 20:38:01 +0200

    feat(public): implement film card placeholder

---
+++ src/app/public/home/components/filmcard/filmcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './filmcard.component.html',
+  styleUrl: './filmcard.component.scss'
+})
+export class FilmcardComponent {
+
+}
--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NowfeaturedComponent } from './components/nowfeatured/nowfeatured.component';
+import { FilmcardComponent } from './components/filmcard/filmcard.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [NavbarComponent, NowfeaturedComponent],
+  imports: [NavbarComponent, NowfeaturedComponent, FilmcardComponent],
 })
 export class HomeComponent {}
