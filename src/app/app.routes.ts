eff279eed7c0726a4d575bb43411d05b5f0f08b6
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-03 19:34:13 +0200

    feat(public): create home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { PublicComponent } from './public/public.component';
+import { HomeComponent } from './public/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: PublicComponent, children: [] },
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
+  },
 ];
--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-public',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './public.component.html',
   styleUrl: './public.component.scss',
 })
