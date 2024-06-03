22087d63efcac065f3049cdd4ee6c01bcf4be611
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-03 20:07:07 +0200

    feat(public): implement featured film

---
+++ src/app/public/home/components/nowfeatured/nowfeatured.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nowfeatured',
+  standalone: true,
+  imports: [],
+  templateUrl: './nowfeatured.component.html',
+  styleUrl: './nowfeatured.component.scss',
+})
+export class NowfeaturedComponent {}
--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NowfeaturedComponent } from './components/nowfeatured/nowfeatured.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NowfeaturedComponent],
 })
 export class HomeComponent {}
