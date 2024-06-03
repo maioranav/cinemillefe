c5b6abfc753efc86020c74631739011cab9cadd4
Vincenzo M <vincenzomaiorana@gmail.com>
2024-06-03 19:32:08 +0200

    feat(public): add public facing component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PublicComponent, children: [] },
+];
