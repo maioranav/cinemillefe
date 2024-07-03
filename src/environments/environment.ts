00f77306ccad42d07f01b402bc3fac7097b9311e
Vincenzo <vincenzomaiorana@gmail.com>
2024-07-03 05:21:13 +0200

    chore: implement interface on environment config file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
-export const environment = {
+export const environment: IEnvironment = {
   API_BASE_URL: 'http://localhost:8080/api/v1/',
 };
+
+export interface IEnvironment {
+  API_BASE_URL: string;
+}
